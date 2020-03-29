9f3172af749ff934622007057f5aaa5498825e46
David Auvray <dauvray@simplon.co>
2020-03-29 21:47:12 +0200

    WIP

--- components/views/Profile/UpdateProfile.vue
+++ components/views/Profile/UpdateProfile.vue
@@ -31,10 +31,37 @@
                     <forgotten-password-form></forgotten-password-form>
                 </div>
                 <div class="tab-pane fade" id="avatar" role="tabpanel" aria-labelledby="avatar-tab">
+
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="upload-example">
+                                <div :style="{backgroundImage: 'url(' + image + ')'}" class="cropper-background"></div>
+                                <cropper
+                                    classname="upload-example-cropper"
+                                    :src="image"
+                                    ref="cropper"
+                                    :stencilComponent="$options.components.CircleStencil"
+                                    :stencil-props="{
+                                aspectRatio: 10/10
+                            }"></cropper>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <img :src="croppedImage" style="max-width: 100%;"/>
+                        </div>
+                    </div>
+
                     <form @submit.prevent="submitForm">
-                        <input type="file" name="filename" placeholder="Sélectionnez un fichier" @change="onFileChange">
-                        <input type="submit" class="btn btn-primary" value="Valider">
+                        <div class="button-wrapper">
+                            <span class="button" @click="$refs.file.click()">
+                                <input type="file" ref="file" @change="uploadImage($event)" accept="image/*">
+                                1 - Sélectionner une image
+                            </span>
+                            <input type="button" class="btn btn-secondary" @click="crop" value="2 - Redimensionner" />
+                            <input type="submit" class="btn btn-primary" value="3 - Valider" />
+                        </div>
                     </form>
+
                 </div>
             </div>
 
@@ -48,20 +75,33 @@
     import { mapActions, mapGetters} from 'vuex'
     import {BaseMixin} from 'vuejs-estarter/mixins/BaseMixin'
     import {FormMixin} from 'vuejs-estarter/mixins/FormMixin'
+    import { Cropper, CircleStencil } from 'vue-advanced-cropper'
 
     export default {
         name: 'UpdateProfile',
         mixins: [BaseMixin, FormMixin],
         components: {
-            ForgottenPasswordForm: () => import('vuejs-estarter/components/widgets/form/ForgottenPassword')
+            ForgottenPasswordForm: () => import('vuejs-estarter/components/widgets/form/ForgottenPassword'),
+            Cropper,
+            CircleStencil
         },
         data() {
             return {
+
+                image: null,
+                croppedImage: null,
+                coordinates: {
+                    width: 0,
+                    height: 0,
+                    left: 0,
+                    top: 0
+                },
+
+
+
                 model: {
                     name: '',
                     email: '',
-                    // password: '',
-                    // password_confirmation: '',
                 },
                 schema: {
                     groups: [
@@ -90,33 +130,6 @@
                         }
                     ],
                 },
-                // schema2: {
-                //     groups: [
-                //         {
-                //             legend: "Gestion du mot de passe",
-                //             fields: [
-                //                 {
-                //                     type: 'passwordChecker',
-                //                     inputName: 'password',
-                //                     label: 'Mot de passe',
-                //                     model: 'password',
-                //                     required: false,
-                //                     isCheckstrength: true,
-                //                     hint: 'Laisser vide si vous ne souhaitez pas modifier votre mot de passe'
-                //                 },
-                //                 {
-                //                     type: 'passwordChecker',
-                //                     inputName: 'password-confirm',
-                //                     label: 'Confirmation du mot de passe',
-                //                     model: 'password_confirmation',
-                //                     required: false,
-                //                     isCheckstrength: false,
-                //                     hint: 'Laisser vide si vous ne souhaitez pas modifier votre mot de passe'
-                //                 }
-                //             ]
-                //         }
-                //     ],
-                // },
                 file: ''
             }
         },
@@ -153,17 +166,89 @@
                     }
                 })
             },
-            // Password
+
 
             // Avatar
-            onFileChange(e) {
-                this.file = e.target.files[0];
-            },
             submitForm(e) {
                 let formData = new FormData();
                 formData.append('file', this.file);
                 this['auth/updateAvatar'](formData)
+            },
+            crop() {
+                const {coordinates, canvas} = this.$refs.cropper.getResult()
+                this.coordinates = coordinates
+                // You able to do different manipulations at a canvas
+                // but there we just get a cropped image
+                this.croppedImage = canvas.toDataURL()
+
+                fetch(this.croppedImage)
+                .then(res => res.blob())
+                .then(blob => {
+                    this.file = new File([blob], "File name",{ type: "image/png" })
+                })
+            },
+            uploadImage(event) {
+                // Reference to the DOM input element
+                var input = event.target;
+                // Ensure that you have a file before attempting to read it
+                if (input.files && input.files[0]) {
+                    // create a new FileReader to read this image and convert to base64 format
+                    var reader = new FileReader();
+                    // Define a callback function to run, when FileReader finishes its job
+                    reader.onload = (e) => {
+                        // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+                        // Read image as base64 and set to imageData
+                        this.image = e.target.result;
+                    }
+                    // Start the reader job - read file as a data url (base64 format)
+                    reader.readAsDataURL(input.files[0]);
+                }
             }
         }
     }
 </script>
+
+<style>
+    .upload-example {
+        position: relative;
+        background: black;
+    }
+
+    .cropper-background {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: 50%;
+        filter: blur(5px);
+        opacity: 0.25;
+    }
+
+    .upload-example-cropper {
+        border: solid 1px #EEE;
+        height: 300px;
+        width: 100%;
+    }
+
+    .button-wrapper {
+        display: flex;
+        justify-content: left;
+    }
+
+    .button {
+        color: white;
+        font-size: 16px;
+        padding: 10px 20px;
+        background: #3fb37f;
+        cursor: pointer;
+        transition: background 0.5s;
+    }
+
+    .button:hover {
+        background: #38d890;
+    }
+
+    .button input {
+        display: none;
+    }
+</style>
