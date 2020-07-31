2e4c8754155ea38d5b5fa19a1aa84498c8985ffa
David Auvray <dauvray@simplon.co>
2020-07-31 10:11:55 +0200

    WIP

--- components/widgets/Modal.vue
+++ components/widgets/Modal.vue
@@ -1,158 +1,47 @@
 <template>
-    <!--  How to :
-    <template>
-        <button
-            type="button"
-            class="btn"
-            @click="showModal"
-        > Open Modal!</button>
-        <modal
-            v-show="isModalVisible"
-            @close="closeModal"
-        />
-        </template>
-
-        <script>
-         data: { isModalVisible: false, } ,
-         methods: {
-            showModal() {
-                this.isModalVisible = true;
-            },
-            closeModal() {
-                this.isModalVisible = false;
-            }
-        }
-        </script>
-    -->
-    <transition name="modal-fade">
-        <div class="modal-backdrop">
-            <div class="modal"
-                 role="dialog"
-                 aria-labelledby="modalTitle"
-                 aria-describedby="modalDescription"
-            >
-                <header
-                    class="modal-header"
-                    id="modalTitle"
-                >
-                    <slot name="header">
-                        This is the default tile!
-
-                        <button
-                            type="button"
-                            class="btn-close"
-                            @click="close"
-                            aria-label="Close modal"
-                        >
-                            x
+    <div>
+        <button type="button" :class="btnclass" data-toggle="modal" :data-target="`#${target}`">
+            <slot name="button">Open modal</slot>
+        </button>
+        <div class="modal fade" :id="`${target}`" tabindex="-1" role="dialog" :aria-labelledby="`#${target}Label`" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" :id="`#${target}Label`">
+                            <slot name="header">Modal title</slot>
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
                         </button>
-                    </slot>
-                </header>
-                <section
-                    class="modal-body"
-                    id="modalDescription"
-                >
-                    <slot name="body">
-                        I'm the default body!
-                    </slot>
-                </section>
-                <footer class="modal-footer">
-                    <slot name="footer">
-                        I'm the default footer!
-
-                        <button
-                            type="button"
-                            class="btn-green"
-                            @click="close"
-                            aria-label="Close modal"
-                        >
-                            Close me!
-                        </button>
-                    </slot>
-                </footer>
+                    </div>
+                    <div class="modal-body">
+                        <slot name="body">...</slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </slot>
+                    </div>
+                </div>
             </div>
         </div>
-    </transition>
+    </div>
+
 </template>
 
 <script>
-    export default {
-        name: 'Modal',
-        methods: {
-            close() {
-                this.$emit('close');
-            },
+export default {
+    name: "Modal",
+    props: {
+        target: {
+            type: String,
+            defaut: "exampleModal"
         },
-    };
+        btnclass: {
+            type: String,
+            default: "btn btn-primary"
+        }
+    },
+}
 </script>
-
-<style>
-    .modal-backdrop {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-color: rgba(0, 0, 0, 0.3);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .modal {
-        background: #FFFFFF;
-        box-shadow: 2px 2px 20px 1px;
-        overflow-x: auto;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .modal-header,
-    .modal-footer {
-        padding: 15px;
-        display: flex;
-    }
-
-    .modal-header {
-        border-bottom: 1px solid #eeeeee;
-        color: #4AAE9B;
-        justify-content: space-between;
-    }
-
-    .modal-footer {
-        border-top: 1px solid #eeeeee;
-        justify-content: flex-end;
-    }
-
-    .modal-body {
-        position: relative;
-        padding: 20px 10px;
-    }
-
-    .btn-close {
-        border: none;
-        font-size: 20px;
-        padding: 20px;
-        cursor: pointer;
-        font-weight: bold;
-        color: #4AAE9B;
-        background: transparent;
-    }
-
-    .btn-green {
-        color: white;
-        background: #4AAE9B;
-        border: 1px solid #4AAE9B;
-        border-radius: 2px;
-    }
-
-    .modal-fade-enter,
-    .modal-fade-leave-active {
-        opacity: 0;
-    }
-
-    .modal-fade-enter-active,
-    .modal-fade-leave-active {
-        transition: opacity .5s ease
-    }
-</style>
