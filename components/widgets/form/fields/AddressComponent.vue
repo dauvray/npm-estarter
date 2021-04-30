35d8b88621457d98505ffbdd55c1ee61ce9e4904
David Auvray <d.auvray@ghu-paris.fr>
2021-04-30 12:10:15 +0200

    Correction addressComponent

--- components/widgets/form/fields/AddressComponent.vue
+++ components/widgets/form/fields/AddressComponent.vue
@@ -6,34 +6,48 @@
 </template>
 
 <script>
-    import { abstractField } from "vue-form-generator"
-    import {get as objGet, isFunction} from "lodash";
-    const places = require('places.js');
+import { abstractField } from "vue-form-generator"
+import {get as objGet, isFunction} from "lodash";
+const places = require('places.js');
 
-    export default {
-        name: 'AddressComponent',
-        mixins: [abstractField],
-        data() {
-            return {
-                hint: '',
-                AddressInputId: `address${Math.floor(Math.random() * Date.now())}`
-            }
-        },
-        mounted() {
-            let placesAutocomplete = places({
-                appId: this.schema.appId,
-                apiKey: this.schema.apiKey,
-                container: document.querySelector(`#${this.AddressInputId}`)
-            }).configure({
-                countries: ['fr']
-            })
-            placesAutocomplete.on('change', (e) => {
-                this.updateModelValue(e.suggestion.value)
-            })
+export default {
+    name: 'AddressComponent',
+    mixins: [abstractField],
+    data() {
+        return {
+            hint: '',
+            AddressInputId: `address${Math.floor(Math.random() * Date.now())}`
+        }
+    },
 
+    mounted() {
+        let placesAutocomplete = places({
+            appId: this.schema.appId,
+            apiKey: this.schema.apiKey,
+            container: document.querySelector(`#${this.AddressInputId}`)
+        }).configure({
+            countries: ['fr']
+        })
+        placesAutocomplete.on('change', (e) => {
+            this.updateModelValue({
+                address: e.suggestion.value,
+                geo: e.suggestion.latlng
+
+            })
+        })
+        placesAutocomplete.on('clear', (e) => {
+            this.updateModelValue({
+                address: '',
+                geo: {}
+
+            })
+        })
+        setTimeout(() => {
             if(this.model && this.model[this.schema.model]) {
-                placesAutocomplete.setVal(this.model[this.schema.model].value || this.model[this.schema.model])
+                // le premier argument est pour les anciennes versions ( obsolete, disparaitra bientot )
+                placesAutocomplete.setVal(this.model[this.schema.model].value || this.model[this.schema.model].address)
             }
-        },
-    }
+        }, 500);
+    },
+}
 </script>
