dd5c680ede1b063036f1e00c11c601212a3d4020
David Auvray <d.auvray@ghu-paris.fr>
2022-02-25 14:47:48 +0100

    WIP

--- state/modules/me/mutations.js
+++ state/modules/me/mutations.js
@@ -2,4 +2,8 @@ export default {
     setMe(state, user) {
         state.me = {...user}
     },
+    // add data from other packages
+    composeMe(state, user) {
+        state.me = {...state.me, ...user}
+    }
 }
